d33165f199ebaffadbb55d13b98ed38f05e1aeab
Damla Koksal <me@damlakoksal.com>
2022-04-20 11:05:12 +0300

    feat: add hooks

---
+++ src/hooks/useEthTransactions.ts
@@ -0,0 +1,21 @@
+// export const useFetch = (url, initialValue) => {
+//   const [data, setData] = useState(initialValue);
+//   const [loading, setLoading] = useState(true);
+//   const fetchData = useCallback(async () => {
+//     try {
+//       setLoading(true);
+//       const response = await axios.get(url);
+//       if (response.status === 200) {
+//         setData(response.data);
+//       }
+//     } catch (error) {
+//       throw error;
+//     } finally {
+//       setLoading(false);
+//     }
+//   }, [url]);
+//   useEffect(() => {
+//     fetchData();
+//   }, [fetchData]);
+//   return { loading, data };
+// };
