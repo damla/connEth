0bfc0c45f0729a5ed2f27844819e6867c4f0916d
Damla Koksal <me@damlakoksal.com>
2022-04-20 12:44:40 +0300

    [#10]feat: pull data from db

--- src/components/templates/HomePage/HomePage.tsx
+++ src/components/templates/HomePage/HomePage.tsx
@@ -1,8 +1,13 @@
+import { Page } from '@prisma/client';
 import { useRouter } from 'next/router';
 
-const HomePage = () => {
-  const router = useRouter();
+interface Props {
+  data: Page['content'];
+}
 
+const HomePage = ({ data }: Props) => {
+  const router = useRouter();
+  console.log(data);
   return (
     <div className="p-6 max-w-sm bg-white rounded-xl shadow-md flex items-center space-x-4 m-20">
       <h3 className="text-lg md:text-xl font-medium text-black">Language</h3>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
-import type { NextPage } from 'next';
+import { Page } from '@prisma/client';
 import { Landing } from '../components/layouts';
 import { HomePage } from '../components/templates';
+import { prisma } from '../lib/prisma/prisma';
 
-const Home: NextPage = () => {
+interface Props {
+  content: Page['content'];
+}
+
+export default function Home({ content }: Props) {
   return (
     <Landing>
-      <HomePage />
+      <HomePage data={content} />
     </Landing>
   );
-};
+}
 
-export default Home;
+export const getServerSideProps = async () => {
+  const content = await prisma.page.findMany({
+    select: {
+      content: true,
+    },
+  });
+  return {
+    props: { content },
+  };
+};
