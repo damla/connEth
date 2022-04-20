9e3dbc78dcac5c11f29885dd6edeef7b2b668bc7
Damla Koksal <me@damlakoksal.com>
2022-04-20 11:11:44 +0300

    fix: add TODO comment

--- src/hooks/useEthTransactions.ts
+++ src/hooks/useEthTransactions.ts
@@ -1,21 +1,22 @@
-// export const useFetch = (url, initialValue) => {
-//   const [data, setData] = useState(initialValue);
-//   const [loading, setLoading] = useState(true);
-//   const fetchData = useCallback(async () => {
-//     try {
-//       setLoading(true);
-//       const response = await axios.get(url);
-//       if (response.status === 200) {
-//         setData(response.data);
-//       }
-//     } catch (error) {
-//       throw error;
-//     } finally {
-//       setLoading(false);
-//     }
-//   }, [url]);
-//   useEffect(() => {
-//     fetchData();
-//   }, [fetchData]);
-//   return { loading, data };
-// };
+export const useEthTransactions = () => {
+  // TODO: Add useEthTransactions hook
+  // const [data, setData] = useState(initialValue);
+  // const [loading, setLoading] = useState(true);
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await axios.get(url);
+  //     if (response.status === 200) {
+  //       setData(response.data);
+  //     }
+  //   } catch (error) {
+  //     throw error;
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, [url]);
+  // useEffect(() => {
+  //   fetchData();
+  // }, [fetchData]);
+  // return { loading, data };
+};
